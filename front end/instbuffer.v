82a82812e65db3f840aa5a52e8c201ba2a39bc91
Ucraneko <1050204780@qq.com>
2024-04-19 19:48:42 +0800

    修复了instbuffer的bug，写了instbuffer的testbench，通过了测试

--- front end/instbuffer.v
+++ front end/instbuffer.v
@@ -28,11 +28,12 @@ module instbuffer(
     input rst,
     input flush,
 
-    //if_unit给的
+    //bpu传来的信号
     input [`InstBus] inst_1_i,
     input [`InstBus] inst_2_i,
     input [`InstBus] pc_1_i,
     input [`InstBus] pc_2_i,
+
     input is_inst1_valid,
     input is_inst2_valid,
 
@@ -40,7 +41,7 @@ module instbuffer(
     input send_inst_1_en,
     input send_inst_2_en,
 
-    //从if_unit取指令的使能信号
+    //从bpu取指令的使能信号
     input fetch_inst_1_en,
     input fetch_inst_2_en,
 
@@ -69,25 +70,31 @@ module instbuffer(
             FIFO_valid <= `InstBufferSize'd0;
         end
         else begin
-            if(fetch_inst_1_en) begin
+            if(fetch_inst_1_en&&fetch_inst_2_en) begin
                 FIFO_inst[tail] <= inst_1_i;
                 FIFO_pc[tail] <= pc_1_i;
                 FIFO_valid[tail] <= 1'b1;
-                tail <= tail + 1;
+                FIFO_inst[tail+1] <= inst_2_i;
+                FIFO_pc[tail+1] <= pc_2_i;
+                FIFO_valid[tail+1] <= 1'b1;
+                tail <= tail + 2;
             end else begin
-                FIFO_inst[tail] <= FIFO_inst[tail];
-                FIFO_pc[tail] <= FIFO_pc[tail];
-                tail <= tail;
-            end
-            if(fetch_inst_2_en) begin
-                FIFO_inst[tail] <= inst_2_i;
-                FIFO_pc[tail] <= pc_2_i;
-                FIFO_valid[tail] <= 1'b1;
-                tail <= tail +1;
-            end else begin
-                FIFO_inst[tail] <= FIFO_inst[tail];
-                FIFO_pc[tail] <= FIFO_pc[tail];
-                tail <= tail;
+                if(fetch_inst_1_en) begin
+                    FIFO_inst[tail] <= inst_1_i;
+                    FIFO_pc[tail] <= pc_1_i;
+                    FIFO_valid[tail] <= 1'b1;
+                    tail <= tail + 1;
+                end else if(fetch_inst_2_en) begin
+                    FIFO_inst[tail] <= inst_2_i;
+                    FIFO_pc[tail] <= pc_2_i;
+                    FIFO_valid[tail] <= 1'b1;
+                    tail <= tail + 1;
+                end else begin
+                    FIFO_inst[tail] <= FIFO_inst[tail];
+                    FIFO_pc[tail] <= FIFO_pc[tail];
+                    FIFO_valid[tail] <= FIFO_valid[tail];
+                    tail <= tail;
+                end
             end
         end
     end
@@ -100,23 +107,23 @@ module instbuffer(
             pc_2_o <= 0;
         end
         else begin
-            if(send_inst_1_en && FIFO_valid[head]) begin
+            if(send_inst_1_en && send_inst_2_en) begin
                 instbuffer_1_o <= FIFO_inst[head];
                 pc_1_o <= FIFO_pc[head];
+                instbuffer_2_o <= FIFO_inst[head+1];
+                pc_2_o <= FIFO_pc[head+1];
+                head <= head + 2;
+            end else if(send_inst_1_en|send_inst_2_en) begin
+                instbuffer_1_o <= FIFO_inst[head];
+                pc_1_o <= FIFO_pc[head];
+                instbuffer_2_o <= 0;
+                pc_2_o <= 0;
                 head <= head + 1;
             end else begin
-                instbuffer_1_o <= instbuffer_1_o;
-                pc_1_o <= pc_1_o;
-                head <= head;
-            end
-            if(send_inst_2_en && FIFO_valid[head]) begin
-                instbuffer_2_o <= FIFO_inst[head];
-                pc_2_o <= FIFO_pc[head];
-                head <= head + 1;
-            end else begin
-                instbuffer_2_o <= instbuffer_2_o;
-                pc_2_o <= pc_2_o;
-                head <= head;
+                instbuffer_1_o <= 0;
+                instbuffer_2_o <= 0;
+                pc_1_o <= 0;
+                pc_2_o <= 0;
             end
         end
     end
