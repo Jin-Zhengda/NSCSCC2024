cb96698009dd532279a7b5ae57295d5252ead5a9
SoaringEagle910 <soaringeagle910@gmail.com>
2024-07-10 11:24:44 +0800

    hhh

--- interface.sv
+++ interface.sv
@@ -221,6 +221,112 @@ interface ctrl_csr;
     );
 endinterface : ctrl_csr
 
+
+interface icache_transaddr;
+    logic                   inst_fetch;    //指令地址转换信息有效的信号assign fetch_en  = inst_valid && inst_addr_ok;
+    logic [31:0]            inst_vaddr;    //虚拟地址
+    logic [31:0]            ret_inst_paddr;//物理地址
+
+    modport master(
+        input ret_inst_paddr,
+        output inst_fetch,inst_vaddr  
+    );
+
+    modport slave(
+        output ret_inst_paddr,
+        input inst_fetch,inst_vaddr
+    );
+endinterface : icache_transaddr
+
+interface dcache_transaddr;
+    logic                   data_fetch;    //指令地址转换信息有效的信号assign fetch_en  = inst_valid && inst_addr_ok;
+    logic [31:0]            data_vaddr;    //虚拟地址
+    logic [31:0]            ret_data_paddr;//物理地址
+    logic                   cacop_op_mode_di;//assign cacop_op_mode_di = ms_cacop && ((cacop_op_mode == 2'b0) || (cacop_op_mode == 2'b1));
+
+    modport master(
+        input ret_data_paddr,
+        output data_fetch,data_vaddr,cacop_op_mode_di
+    );
+
+    modport slave(
+        output ret_data_paddr,
+        input data_fetch,data_vaddr,cacop_op_mode_di
+    );
+endinterface : dcache_transaddr
+
+
+
+interface ex_tlb;
+    //TLBFILL和TLBWR指令
+    logic                  tlbfill_en         ;//TLBFILL指令的使能信号
+    logic                  tlbwr_en           ;//TLBWR指令的使能信号
+
+    //TLBSRCH指令
+    logic                  tlbsrch_en         ;//TLBSRCH指令使能信号
+
+    //TLBRD指令（输入的信号复用tlbidx_in），下一周期开始返回读取的结果
+    //默认read
+
+    //invtlb ——用于实现无效tlb的指令
+    logic                  invtlb_en          ;//使能
+    logic [ 9:0]           invtlb_asid        ;//asid
+    logic [18:0]           invtlb_vpn         ;//vpn
+    logic [ 4:0]           invtlb_op          ;//op
+
+
+    modport master(//ex
+        output tlbfill_en,tlbwr_en,tlbsrch_en,invtlb_en,invtlb_asid,invtlb_vpn,invtlb_op
+    );
+    modport slave(//tlb
+        input tlbfill_en,tlbwr_en,tlbsrch_en,invtlb_en,invtlb_asid,invtlb_vpn,invtlb_op
+    );
+endinterface //ex_tlb
+
+
+interface csr_tlb;
+    logic [31:0]           tlbidx             ;//7.5.1TLB索引寄存器，包含[4:0]为index,[29:24]为PS，[31]为NE
+    logic [31:0]           tlbehi             ;//7.5.2TLB表项高位，包含[31:13]为VPPN
+    logic [31:0]           tlbelo0,tlbelo1    ;//7.5.3TLB表项低位，包含写入TLB表项的内容
+    logic [ 9:0]           asid               ;//7.5.4ASID的低9位
+    //TLBFILL和TLBWR指令
+    logic [4:0]            rand_index         ;//TLBFILL指令的index
+    logic [5:0]            ecode           ;//7.5.1对于NE变量的描述中讲到，CSR.ESTAT.Ecode   (大概使能信号，若为111111则写使能，否则根据tlbindex_in.NE判断是否写使能？
+
+    //TLBSRCH指令
+    logic [31:0]           tlbsrch_ehi        ;//TLBSRCH指令的ehi信号
+    logic                  search_tlb_found   ;//TLBSRCH命中
+    logic [ 4:0]           search_tlb_index   ;//TLBSRCH所需返回的index信号
+
+    //TLBRD指令（输入的信号复用tlbidx_in），下一周期开始返回读取的结果
+    logic [31:0]           tlbehi_out         ;//{r_vppn, 13'b0}
+    logic [31:0]           tlbelo0_out        ;//{4'b0, ppn0, 1'b0, g, mat0, plv0, d0, v0}
+    logic [31:0]           tlbelo1_out        ;//{4'b0, ppn1, 1'b0, g, mat1, plv1, d1, v1}
+    logic [31:0]           tlbidx_out         ;//只有[29:24]为ps信号，其他位均为0
+    logic [ 9:0]           asid_out           ;//读出的asid
+
+    //CSR信号
+    logic [31:0]           csr_dmw0           ;//dmw0，有效位是[27:25]，可能会作为最后转换出来的地址的最高三位
+    logic [31:0]           csr_dmw1           ;//dmw1，有效位是[27:25]，可能会作为最后转换出来的地址的最高三位
+    logic                  csr_da             ;
+    logic                  csr_pg             ;   
+    logic [1:0]            csr_plv            ;
+
+    modport master(//csr
+        input search_tlb_found,search_tlb_index,tlbehi_out,tlbelo0_out,tlbelo1_out,tlbidx_out,asid_out,
+        output tlbidx,tlbehi,tlbelo0,tlbelo1,asid,rand_index,ecode,tlbsrch_ehi,
+                csr_dmw0,csr_dmw1,csr_da,csr_pg,csr_plv
+    );
+    modport slave(//tlb
+        input tlbidx,tlbehi,tlbelo0,tlbelo1,asid,rand_index,ecode,tlbsrch_ehi,
+                csr_dmw0,csr_dmw1,csr_da,csr_pg,csr_plv,
+        output search_tlb_found,search_tlb_index,tlbehi_out,tlbelo0_out,tlbelo1_out,tlbidx_out,asid_out
+    );
+
+endinterface //csr_tlb
+
+
+
 interface icache_mem;
     logic rd_req, ret_valid;
     bus32_t  rd_addr;
